886dfc13d4c43affdfce0df9eb14bb62dee08f0d
Gautam Peri <naperi@microsoft.com>
2024-08-02 14:33:32 -0700

    Minor fixes

--- csharp-challenges/csharp-code-for-challenges/CSharp-Challenge2.cs
+++ csharp-challenges/csharp-code-for-challenges/CSharp-Challenge2.cs
@@ -6,7 +6,7 @@ namespace CSharpChallenges
     {
         [FunctionName("GetData")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -19,31 +19,35 @@ namespace CSharpChallenges
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
             return new OkObjectResult(responseMessage);
         }
-        [FunctionName("GetResults")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-            string name = req.Query["name"];
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-            return new OkObjectResult(responseMessage);
-        }
+
         [FunctionName("UpdateDatabase")]
         public void Run([ServiceBusTrigger("myqueue", Connection = "")] string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
         }
+
         [FunctionName("DeleteCache")]
         public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
         }
+
+        [FunctionName("SaveData")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest request,
+            ILogger logger)
+        {
+            logger.LogInformation("C# HTTP trigger function processed a request.");
+            string name = request.Query["name"];
+            string requestBody = await new StreamReader(request.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            return new OkObjectResult(responseMessage);
+        }
+
 
     }
 }
